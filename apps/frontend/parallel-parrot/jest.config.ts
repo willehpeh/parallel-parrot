af474ebb90568672d727c817dbb3c060e0a0c658
w.alexander <will_alexander@me.com>
2025-04-20 00:09:44 +0200

    Use NX tools to move frontend projects to own folder

--- apps/frontend/parallel-parrot/jest.config.ts
+++ apps/frontend/parallel-parrot/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
   displayName: 'parallel-parrot',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/parallel-parrot',
+  coverageDirectory: '../../../coverage/apps/frontend/parallel-parrot',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
