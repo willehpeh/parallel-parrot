12aeafdbe2e1648e80165e802be23a1bd36974e5
w.alexander <will_alexander@me.com>
2025-04-21 00:06:14 +0200

    Serve static, add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:20.15.0-alpine AS builder
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npx nx build parallel-parrot-api
+RUN npx nx build parallel-parrot
+FROM node:20.15.0-alpine as runner
+COPY --from=builder /app/dist/apps/backend/parallel-parrot-api ./
+COPY --from=builder /app/dist/apps/frontend/parallel-parrot/browser ./public/
+RUN npm install --omit=dev
+EXPOSE 3000
--- apps/backend/parallel-parrot-api/src/app/app.module.ts
+++ apps/backend/parallel-parrot-api/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'public'),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
