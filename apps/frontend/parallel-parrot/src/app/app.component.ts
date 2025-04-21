b7bb7a9f70f1deec40f1feff044d53bb5f5e1a9a
w.alexander <will_alexander@me.com>
2025-04-21 23:20:33 +0200

    Remove test title

--- apps/frontend/parallel-parrot/src/app/app.component.ts
+++ apps/frontend/parallel-parrot/src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   imports: [],
   selector: 'app-root',
-  template: `
-    <h1>Parallel Parrot</h1>
-  `,
+  template: ``,
 })
 export class AppComponent {}
